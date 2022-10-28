41b50a9edb5f896c28b241db5baeed9cc275ecd3
Lilibet Fernandez <lilibetfernandez@Lilibets-MacBook-Pro.local>
2022-10-28 19:23:25 +0200

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,69 @@
+import React, { useContext } from 'react';
+import { useRouter } from 'next/router';
+import Image from 'next/image';
+import Menu from '@components/Menu';
+import MyOrder from '@containers/MyOrder';
+import menu from '@icons/icon_menu.svg';
+import logo from '@logos/logo_yard_sale.svg';
+import AppContext from '@context/AppContext';
+import shoppingCart from '@icons/icon_shopping_cart.svg';
+import styles from '@styles/Header.module.scss';
+
+const Header = () => {
+  const { state, toggleOrder, toggleMenu } = useContext(AppContext);
+  const router = useRouter();
+  return (
+    <>
+      <nav className={styles.Nav}>
+        <Image src={menu} alt="menu" className={styles.menu} />
+        <div className={styles['navbar-left']}>
+          <Image
+            src={logo}
+            alt="logo"
+            className={styles['nav-logo']}
+            width={50}
+            height={50}
+            onClick={() => {
+              router.push('/');
+            }}
+          />
+          <ul>
+            <li>
+              <a href="/">All</a>
+            </li>
+            <li>
+              <a href="/">Clothes</a>
+            </li>
+            <li>
+              <a href="/">Electronics</a>
+            </li>
+            <li>
+              <a href="/">Furnitures</a>
+            </li>
+            <li>
+              <a href="/">Toys</a>
+            </li>
+            <li>
+              <a href="/">Others</a>
+            </li>
+          </ul>
+        </div>
+        <div className={styles['navbar-right']}>
+          <ul>
+            <li className={`${styles['more-clickable-area']} ${styles['navbar-email']} ${styles['pointer']}`} onClick={() => toggleMenu()}>
+              platzi@example.com
+            </li>
+            <li className={styles['navbar-shopping-cart']} onClick={() => toggleOrder()}>
+              <Image className={`${styles['more-clickable-area pointer']}`} src={shoppingCart} alt="shopping cart" />
+              {state && state.cart ? state.cart.length > 0 ? <div>{state.cart.length}</div> : null : null}
+            </li>
+          </ul>
+        </div>
+        {state ? state.menuIsOpen && <Menu /> : null}
+      </nav>
+      {state ? state.orderIsOpen && <MyOrder /> : null}
+    </>
+  );
+};
+
+export default Header;
---
+++ src/components/ProductItem.jsx
@@ -0,0 +1,31 @@
+import React, { useContext } from 'react';
+import AppContext from '@context/AppContext';
+import addToCartImage from '@icons/bt_add_to_cart.svg';
+import addedToCartImage from '@icons/bt_added_to_cart.svg';
+import styles from '@styles/ProductItem.module.scss';
+
+const ProductItem = ({ product }) => {
+	const { state, addToCart } = useContext(AppContext);
+
+	const handleClick = item => {
+		console.log('in cart: ', state.cart.includes(item));
+		addToCart(item)
+	}
+
+	return (
+		<div className={styles.ProductItem}>
+			<img src={product.images[0]} alt={product.title} />
+			<div className={styles['product-info']}>
+				<div>
+					<p>${product.price}</p>
+					<p>{product.title}</p>
+				</div>
+				<figure className={styles['more-clickable-area']} onClick={() => handleClick(product)} >
+					{state.cart.includes(product) ? <img className={`${styles.disabled} ${styles['add-to-cart-btn']}`} src={addedToCartImage} alt="added to cart" />  : <img className={`${styles['add-to-cart-btn']} ${styles.pointer}`}src={addToCartImage} alt="add to cart" /> }
+				</figure>
+			</div>
+		</div>
+	);
+}
+
+export default ProductItem;
